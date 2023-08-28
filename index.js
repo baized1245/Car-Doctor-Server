a4613cce720c2cc9e3773a2a4d1df42d3c78aa8a
unknown <you@example.com>
2023-08-29 04:03:27 +0600

    70.6

--- index.js
+++ index.js
@@ -44,16 +44,28 @@ async function run() {
     const query = {_id: new ObjectId(id)}
 
   const options = {
-    projection: {title: 1, price: 1, service_id: 1}
-  }
+    projection: {title: 1, price: 1, service_id: 1, img: 1},
+  };
 
     const result = await serviceCollection.findOne(query, options);
     res.send(result);
   });
 
   // Bookings
+  app.get('/bookings', async(req, res) => {
+    console.log(req.query.email);
+    let query = {};
+     if(req.query?.email){
+       query = { email: req.query.email}
+     }
+    const result = await bookingCollection.find(query).toArray();
+    res.send(result);
+  })
   app.post('/bookings', async(req, res) => {
     const booking = req.body;
+    console.log(booking);
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
   })
 
 
